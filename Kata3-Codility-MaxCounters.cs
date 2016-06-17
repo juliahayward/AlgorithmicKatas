b86423ab04f4339313362962eb879061655ade78
Julia Hayward <julia.hayward@red-gate.com>
2016-06-17 10:01:07 +0100

    kata 3

---
+++ Kata3-Codility-MaxCounters.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace UnitTestProject1
+{
+    // https://codility.com/demo/take-sample-test/max_counters/
+    class Kata3_Codility_MaxCounters
+    {
+        public int[] solution(int N, int[] A)
+        {
+            var counters = new int[N];
+            // Keep a running track of the highest counter so far
+            var maxSoFar = 0;
+            // Keep a running track of where the "max all" would take you to, if applied.
+            var baseValue = 0;
+
+            foreach (int operation in A)
+            {
+                if (operation < N + 1)
+                {
+                    // Danger - counters seem to be 1-based
+
+                    // If we're below baseValue, this is because we have postponed updating it from [1]
+                    if (counters[operation - 1] < baseValue)
+                        counters[operation - 1] = baseValue;
+
+                    counters[operation - 1]++;
+                    maxSoFar = Math.Max(maxSoFar, counters[operation - 1]);
+                }
+                else
+                {
+                    // [1] Every counter should be incremented to baseValue now, but we defer actually doing
+                    // the increment to avoid a potentially O(M*N) result. Counters are left with lower values 
+                    // in them, but this represents baseValue.
+                    baseValue = maxSoFar;
+                }
+            }
+
+            // Finally, fix up all the deferred increments. 
+            for (int i = 0; i < N; i++)
+                if (counters[i] < baseValue)
+                    counters[i] = baseValue;
+
+            return counters;
+        }
+    }
+}
+
---
+++ Kata3-PaintingFences-CodeForces.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+
+namespace UnitTestProject1
+{
+    // http://codeforces.com/contest/448/problem/C
+    [TestFixture]
+    public class Kata3Fence
+    {
+        [TestCase(new[] { 5,2,2,5,1,1,5,2,2,5 }, 7)]
+        [TestCase(new[] {2, 2, 1, 2, 1}, 3)]
+        [TestCase(new[] {2, 2}, 2)]
+        [TestCase(new[] {5}, 1)]
+        public void TestCases(int[] heights, int expected)
+        {
+            var strokes = MinStrokesForFence(heights.ToList());
+            Assert.AreEqual(strokes, expected);
+        }
+
+        public int MinStrokesForFence(List<int> heights)
+        {
+            if (!heights.Any()) return 0;
+
+            // A vertical stroke doesn't help horizontal strokes in any way
+
+            var strokesVerticalOnly = heights.Count(x => x > 0);
+
+            // Any horizontal stroke that's not at the bottom of the remaining area is
+            // clearly sub-optimal, so ignore; we'll assume a bottom-most stroke. Also, 
+            // if horizontal strokes help then we will do enough to complete at least
+            // one post. This divides the fence into separate pieces that are done independently
+
+            var widthOfHorizontalStrokes = heights.Min();
+            var strokesWithOneHorizontalNow = widthOfHorizontalStrokes; // the horizontal stroke
+            var subList = new List<int>();
+            foreach (var height in heights)
+            {
+                if (height > widthOfHorizontalStrokes)
+                    subList.Add(height - widthOfHorizontalStrokes);
+                else
+                {
+                    strokesWithOneHorizontalNow += MinStrokesForFence(subList);
+                    subList.Clear();
+                }
+            }
+            strokesWithOneHorizontalNow += MinStrokesForFence(subList);
+
+            return Math.Min(strokesVerticalOnly, strokesWithOneHorizontalNow);
+        }
+
+
+
+    }
+}
